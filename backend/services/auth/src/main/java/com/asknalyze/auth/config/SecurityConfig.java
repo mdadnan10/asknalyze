2f0a654be0322b286233c3b6a50d73d91a2e05b5
adnan <10.mdadnan@gmail.com>
2025-07-31 23:08:23 +0530

    SecurityConfig updated.

--- backend/services/auth/src/main/java/com/asknalyze/auth/config/SecurityConfig.java
+++ backend/services/auth/src/main/java/com/asknalyze/auth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.asknalyze.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,9 +13,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/dashboard/**").authenticated()
                         .anyRequest().permitAll()
                 )
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
