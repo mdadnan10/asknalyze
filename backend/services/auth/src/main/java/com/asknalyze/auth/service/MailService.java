32f8b284c11bea7df484334fa43128645c01b63c
adnan <10.mdadnan@gmail.com>
2025-08-07 00:48:21 +0530

    Asynchronously mail sending feature added.

--- backend/services/auth/src/main/java/com/asknalyze/auth/service/MailService.java
+++ backend/services/auth/src/main/java/com/asknalyze/auth/service/MailService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,8 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+
+    @Async
     public void sendEmail(MailRequest request) {
         String to = request.getSendTo();
         String context = request.getContext();
